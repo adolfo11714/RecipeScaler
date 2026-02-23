22676a89f9eab29d1bc7f0647296fe0611608f67
adolfo11714 <adolfomartinez122@yahoo.com>
2026-02-22 21:17:29 -0500

    menu is now centered and buttons can be sized differently

--- src/com/adolfomartinez/recipescaler/panels/MainMenuPanel.java
+++ src/com/adolfomartinez/recipescaler/panels/MainMenuPanel.java
@@ -1,36 +1,69 @@
 package com.adolfomartinez.recipescaler.panels;
 
 import com.adolfomartinez.recipescaler.MainFrame;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import javax.swing.*;
 
 public class MainMenuPanel extends JPanel {
     public MainMenuPanel(MainFrame frame) {
+
+        setLayout(new BorderLayout());
+
+        JPanel menu = new JPanel();
+        menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
+        menu.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+
+        Dimension buttonSize = new Dimension(400, 70);
+
         JButton secondButton = new JButton("Go to Second Screen");
         JButton thirdButton = new JButton("Go to Third Screen");
         JButton fourthButton = new JButton("Go to Fourth Screen");
         JButton fifthButton = new JButton("Go to Fifth Screen");
 
+        // Center Buttons (BoxLayout needs this)
+        secondButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        thirdButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        fourthButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        fifthButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        secondButton.addActionListener(e -> {
-            frame.showScreen("SECOND");
-        });
+        // Center vertically
+        menu.add(Box.createVerticalGlue());
+        menu.add(secondButton);
+        menu.add(Box.createVerticalStrut(15));
+        menu.add(thirdButton);
+        menu.add(Box.createVerticalStrut(15));
+        menu.add(fourthButton);
+        menu.add(Box.createVerticalStrut(15));
+        menu.add(fifthButton);
+        menu.add(Box.createVerticalGlue());
 
-        thirdButton.addActionListener(e -> {
-            frame.showScreen("THIRD");
-        });
+        add(menu, BorderLayout.CENTER);
 
-        fourthButton.addActionListener(e -> {
-            frame.showScreen("FOURTH");
-        });
+        // Set button size
+        secondButton.setPreferredSize(buttonSize);
+        secondButton.setMaximumSize(buttonSize);
+        secondButton.setMinimumSize(buttonSize);
 
-        fifthButton.addActionListener(e -> {
-            frame.showScreen("FIFTH");
-        });
+        thirdButton.setPreferredSize(buttonSize);
+        thirdButton.setMaximumSize(buttonSize);
+        thirdButton.setMinimumSize(buttonSize);
 
-        add(secondButton);
-        add(thirdButton);
-        add(fourthButton);
-        add(fifthButton);
+        fourthButton.setPreferredSize(buttonSize);
+        fourthButton.setMaximumSize(buttonSize);
+        fourthButton.setMinimumSize(buttonSize);
 
+        fifthButton.setPreferredSize(buttonSize);
+        fifthButton.setMaximumSize(buttonSize);
+        fifthButton.setMinimumSize(buttonSize);
+
+        secondButton.addActionListener(e -> frame.showScreen("SECOND"));
+
+        thirdButton.addActionListener(e -> frame.showScreen("THIRD"));
+
+        fourthButton.addActionListener(e -> frame.showScreen("FOURTH"));
+
+        fifthButton.addActionListener(e -> frame.showScreen("FIFTH"));
     }
 }
